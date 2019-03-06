49ddb389f58cd1e0439b823f9636460b5e4f7909
pasekdawid <poczta@pasekdawid.pl>
2019-03-06 04:47:53 +0100

    Add form component

---
+++ src/app/formTemplates/formTemplate.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'fb-formTemplates',
+    templateUrl:'./formTemplate.component.html'
+})
+
+export class FormTemplateComponent{
+    pageTitle: string = 'Build your own form!';
+}
